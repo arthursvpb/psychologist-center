fe85ea563d895a2380ef0f67670503987f235ea9
arthursvpb <arthurandroid15@gmail.com>
2022-08-31 03:15:26 -0300

    add patients page

--- web/src/pages/dahsboard/index.jsx
+++ web/src/pages/dahsboard/index.jsx
@@ -1,4 +1,4 @@
-import { Col, Row } from 'antd';
+import { Col, Divider, Row } from 'antd';
 import { Card } from '../../components/Card';
 
 export const Dashboard = () => {
@@ -6,6 +6,8 @@ export const Dashboard = () => {
     <>
       <h1>Dashboard</h1>
 
+      <Divider />
+      
       <Row gutter={[16, 16]}>
         <Col>
           <Card title="Sessions scheduled (day)" number="4" />
--- web/src/pages/patients/index.jsx
+++ web/src/pages/patients/index.jsx
@@ -1,3 +1,95 @@
+import './styles.scss';
+
+import { Button, Divider, Space, Table } from 'antd';
+
+import { DeleteFilled, EditFilled, EyeFilled } from '@ant-design/icons';
+
+const columns = [
+  {
+    title: 'Name',
+    width: 75,
+    dataIndex: 'name',
+    key: 'name',
+    fixed: 'left',
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: '1',
+    width: 75,
+  },
+  {
+    title: 'E-mail',
+    dataIndex: 'email',
+    key: '2',
+    width: 75,
+  },
+  {
+    title: 'Gender',
+    dataIndex: 'gender',
+    key: '3',
+    width: 75,
+  },
+
+  {
+    title: 'City',
+    dataIndex: 'city',
+    key: '4',
+    width: 75,
+  },
+  {
+    title: 'Action',
+    key: 'operation',
+    fixed: 'right',
+    width: 40,
+    render: () => (
+      <Space size="middle">
+        <a>
+          <EyeFilled style={{ color: 'var(--blue-400)' }} />
+        </a>
+        <a>
+          <EditFilled style={{ color: 'var(--green-600)' }} />
+        </a>
+        <a>
+          <DeleteFilled style={{ color: 'var(--red-500)' }} />
+        </a>
+      </Space>
+    ),
+  },
+];
+const data = [];
+
+for (let i = 0; i < 100; i++) {
+  data.push({
+    key: i,
+    name: `Harry Potter ${i}`,
+    address: `4 Privet Drive ${i}`,
+    email: `harry@potter.com ${i}`,
+    gender: `Male`,
+    city: `Little Whinging ${i}`,
+  });
+}
+
 export const Patients = () => {
-  return <h1>Patients</h1>;
+  return (
+    <>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+        <h1>My Patients</h1>
+        <Button type="primary">New Patient</Button>
+      </div>
+      <Divider />
+      <Table
+        columns={columns}
+        dataSource={data}
+        summary={() => (
+          <Table.Summary>
+            <Table.Summary.Row>
+              <Table.Summary.Cell index={0} colSpan={2}></Table.Summary.Cell>
+            </Table.Summary.Row>
+          </Table.Summary>
+        )}
+        sticky
+      />
+    </>
+  );
 };
